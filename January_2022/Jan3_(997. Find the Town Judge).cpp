2732a09d92b1a3d0214c7f536d47f7136077d6da
Mohammadtej <tej.mohammad53@gmail.com>
2022-01-03 11:49:58 +0530

    Added January 3 code

---
+++ January_2022/Jan3_(997. Find the Town Judge).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> in(n+1, 0), out(n+1, 0);
+        
+        for(int i=0; i<trust.size(); i++)
+        {
+            in[trust[i][1]]++;
+            out[trust[i][0]]++;
+        }
+        
+        for(int i=1; i<=n; i++)
+        {
+            if(in[i] == (n-1) && out[i] == 0)
+                return i;
+        }
+        
+        return -1;
+    }
+};
